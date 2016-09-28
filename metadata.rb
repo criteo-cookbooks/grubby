e5a86b50528591460c1110f5b691981925da2c66
Jeremy MAURO <j.mauro@criteo.com>
2016-09-28 14:37:52 +0200

    Fixing metadata to use issues_url/source_url only if defined
    Change-Id: Ia9d6c07e197e9dec0ebb337e7da5f33d81ad7e1e

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'j.mauro@criteo.com'
 license          'Apache 2.0'
 description      'Installs/Configures grubby'
 long_description 'Installs/Configures grubby'
-issues_url       'https://github.com/criteo-cookbooks/grubby'
-source_url       'https://github.com/criteo-cookbooks/grubby'
+issues_url       'https://github.com/criteo-cookbooks/grubby' if respond_to?(:issue_url)
+source_url       'https://github.com/criteo-cookbooks/grubby' if respond_to?(:source_url)
 version          '0.1.0'
 supports         'centos'
