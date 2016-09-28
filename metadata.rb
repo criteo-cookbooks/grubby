d69f1884846f44fb9305cefb9225ee4f274dc4c8
Jeremy MAURO <j.mauro@criteo.com>
2016-09-28 14:47:07 +0200

    Adding rhel support
    Change-Id: I9d89e67abeb561a73a946c3acd2062ab8547f0fb

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,6 @@ description      'Installs/Configures grubby'
 long_description 'Installs/Configures grubby'
 issues_url       'https://github.com/criteo-cookbooks/grubby' if respond_to?(:issue_url)
 source_url       'https://github.com/criteo-cookbooks/grubby' if respond_to?(:source_url)
-version          '0.1.0'
+version          '0.1.1'
 supports         'centos'
+supports         'rhel'
